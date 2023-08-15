73daaeaef2a0a0d59750bb76362882bc81b4baa7
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2023-08-15 17:37:29 +0530

    Table state_master
    In SQL state_master table created.

---
+++ src/main/java/com/cdac/fleetmgmt/entities/State_master.java
@@ -0,0 +1,32 @@
+package com.cdac.fleetmgmt.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class State_master {
+	
+	@Id
+	public long stateId;
+	
+	public String stateName;
+
+	public long getStateId() {
+		return stateId;
+	}
+
+	public void setStateId(long stateId) {
+		this.stateId = stateId;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+
+	
+	
+	
+
+}
